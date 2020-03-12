3dc5c3e842800fb9212174af750e85338b7b7d3f
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-03-12 18:44:55 +0530

    Initial commit

---
+++ app/src/main/java/golhar/cocomo/zinger_app/MainActivity.java
@@ -0,0 +1,14 @@
+package golhar.cocomo.zinger_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
