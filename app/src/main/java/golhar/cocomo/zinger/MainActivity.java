d47b151332052b67c01b708783fa36215d1ff1cc
Logesh Dinakaran <ddlogesh@gmail.com>
2020-03-15 10:27:44 +0530

    mainActivity merged

--- app/src/main/java/golhar/cocomo/zinger/MainActivity.java
+++ app/src/main/java/golhar/cocomo/zinger/MainActivity.java
@@ -1,31 +1,56 @@
 package golhar.cocomo.zinger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-import golhar.cocomo.zinger.enums.UserRole;
-import golhar.cocomo.zinger.model.OrderModel;
-import golhar.cocomo.zinger.model.UserCollegeModel;
-import golhar.cocomo.zinger.model.UserModel;
-import golhar.cocomo.zinger.service.MainRepository;
-import golhar.cocomo.zinger.utils.Response;
-import retrofit2.Call;
-import retrofit2.Callback;
+import golhar.cocomo.zinger.adapter.ViewPageAdapter;
+import golhar.cocomo.zinger.fragment.Frag1;
+import golhar.cocomo.zinger.fragment.Frag2;
+import golhar.cocomo.zinger.fragment.Frag3;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView Login_tv;
+    Button location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager viewPager=findViewById(R.id.viewPager);
 
+        ViewPageAdapter adpater=new ViewPageAdapter(getSupportFragmentManager());
 
+        adpater.addFragment(new Frag1());
+        adpater.addFragment(new Frag2());
+        adpater.addFragment(new Frag3());
+
+        viewPager.setAdapter(adpater);
+
+        Login_tv = (TextView) findViewById(R.id.LoginTV);
+        Login_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Login_activity = new Intent(MainActivity.this,Login.class);
+                startActivity(Login_activity);
+                //startActivity(new Intent(this, RecyclerViewDemoActivity.class));
+            }
+        });
+
+        location = (Button) findViewById(R.id.locationB);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent college_loco = new Intent(MainActivity.this,CollegeList.class);
+                startActivity(college_loco);
+            }
+        });
     }
 }
- //comment from mahima
+//comment from mahima
