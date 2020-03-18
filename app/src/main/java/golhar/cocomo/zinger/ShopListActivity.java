324889f1f8f8e5be1a8087120ed57c3d76e92cda
gokhul <gokhulnath18051@cse.ssn.edu.in>
2020-03-18 18:22:07 +0530

    order activity intented to shop list activity

--- app/src/main/java/golhar/cocomo/zinger/ShopListActivity.java
+++ app/src/main/java/golhar/cocomo/zinger/ShopListActivity.java
@@ -1,14 +1,26 @@
 package golhar.cocomo.zinger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShopListActivity extends AppCompatActivity {
 
+    Button accountB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_list);
+        accountB=(Button) findViewById(R.id.accountB);
+        accountB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent order = new Intent(ShopListActivity.this,OrderHistoryActivity.class);
+                startActivity(order);
+            }
+        });
     }
 }
